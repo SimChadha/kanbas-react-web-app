39dea0ff42f5e319ca9375345e7066f32bd23d45
Simar Chadha <simarchadha1@gmail.com>
2023-11-19 21:39:44 -0500

    Added env variables

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -12,7 +12,7 @@ import axios from "axios";
 
 function Courses() {
   const { courseId } = useParams();
-  const URL = "http://localhost:4000/api/courses";
+  const URL = `${process.env.REACT_APP_API_BASE}/courses`;
   const [course, setCourse] = useState({});
   const findCourseById = async (courseId) => {
     const response = await axios.get(
